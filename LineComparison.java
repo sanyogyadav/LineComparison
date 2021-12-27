f6310b98e906cb3ecd2c1b6d8edc9cfe91e71289
Sanyog Yadav <sanyogyadav52@gmail.com>
2021-12-28 03:42:15 +0530

    Wrongly edited some chnages in master branch

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,4 @@
 public class LineComparison {
-
 	public static void main(String arg[]) {
 		int x1, x2, y1, y2;
 		double line1_length;
@@ -8,7 +7,7 @@ public class LineComparison {
 		x2 = 5;
 		y2 = 6;
 		line1_length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("Welcome To Line Comparison");
+		System.out.println("Welcome To Line Comparison Computation");
 		System.out.println("line1 length:" + line1_length);
 	}
 }
