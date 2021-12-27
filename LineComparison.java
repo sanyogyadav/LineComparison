26fe68903abca6a92ed5c7e908f8fd33d9168741
Sanyog Yadav <sanyogyadav52@gmail.com>
2021-12-28 02:18:34 +0530

    This is my first commit in UC1

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,14 @@
 public class LineComparison {
+
 	public static void main(String arg[]) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+		int x1, x2, y1, y2;
+		double line1_length;
+		x1 = 4;
+		y1 = 5;
+		x2 = 5;
+		y2 = 6;
+		line1_length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Welcome To Line Comparison");
+		System.out.println("line1 length:" + line1_length);
 	}
 }
